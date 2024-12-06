0f925d78ac254e0957c8242bb12c104cc60d942e
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-12-06 15:15:56 +0500

    Task3

--- Tyuiu.MedyanichevDI.Sprint6.Task3.V2.Lib/DataService.cs
+++ Tyuiu.MedyanichevDI.Sprint6.Task3.V2.Lib/DataService.cs
@@ -8,17 +8,23 @@ namespace Tyuiu.MedyanichevDI.Sprint6.Task3.V2.Lib
         {
             int row = matrix.GetUpperBound(0) + 1;
             int colm = matrix.Length / row;
-            int con = 0;
+            int[] firstColumn = new int[row];
+            for (int i = 0; i < row; i++)
+            {
+                firstColumn[i] = matrix[i, 0];
+            }
             for (int i = 0; i <=1 ; i++)
             {
-                for (int j = 0; j < colm; j++)
+               
+                if (firstColumn[i] % 2 == 0)
                 {
-                    if (matrix[i, j] % 2 == 0)
-                    {
-                        matrix[i, j] = 0;
-                    }
-                }
+                    firstColumn[i] = 0;
+                } 
 
+            }
+            for (int i = 0; i < row; i++)
+            {
+                matrix[i, 0] = firstColumn[i];
             }
             return matrix;
         }
