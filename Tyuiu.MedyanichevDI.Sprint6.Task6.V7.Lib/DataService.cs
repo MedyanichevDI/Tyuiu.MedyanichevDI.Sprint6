5227f204c499cc19e8353c04969d7a10c5d6a8e7
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-12-10 20:16:31 +0500

    Task6

---
+++ Tyuiu.MedyanichevDI.Sprint6.Task6.V7.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection.PortableExecutable;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.MedyanichevDI.Sprint6.Task6.V7.Lib
+{
+    public class DataService : ISprint6Task6V7
+    {
+        public string CollectTextFromFile(string str, string path)
+        {
+            StringBuilder result = new StringBuilder();
+
+            try
+            {
+                string input;
+                using (var reader = new StreamReader(path))
+                {
+                    input = reader.ReadToEnd();
+                }
+
+
+                var words = input.Split(new[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+               
+
+                var thirdWords = input
+               .Select(line => input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+               .Where(words => words.Length >= 3)
+               .Select(words => words[2]);
+
+                // Объединяем третьи слова в одну строку
+                string res = string.Join(" ", thirdWords);
+                return res;
+               
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ошибка: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ Tyuiu.MedyanichevDI.Sprint6.Task6.V7.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MedyanichevDI.Sprint6.Task6.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
